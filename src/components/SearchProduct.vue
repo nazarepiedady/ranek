f1e90556aa58cc703280695b24e2ae159136d6ec
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-27 17:43:34 +0100

    Create SearchProduct component
    Set the basic markup structure, also bind the
    text in the search input to the search data
    function property.

---
+++ src/components/SearchProduct.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="search">
+    <input
+      name="search"
+      id="search"
+      type="text"
+      v-model="search"
+      class="search__input"
+      placeholder="Busca..."
+    />
+    <button
+      type="submit"
+      id="magnifying-glass"
+      @click.prevent="searchProduct"
+      class="search__button"
+    >
+      Buscar
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
