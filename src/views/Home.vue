7a08d2e8a49380e906e596d6403e85c2ced69315
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-27 15:35:59 +0100

    Create heading for Home view
    Combine text and emoji in the heading,
    add specific style to it using BEM
    methodology, .home__heading to not use
    scoped attribute in style tag, finally
    change the main element from 'div' to
    'section'.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home"></div>
+  <section class="home">
+    <h1 class="home__heading">👍 Compre ou Venda 👎</h1>
+  </section>
 </template>
 
 <script>
@@ -8,3 +10,12 @@ export default {
   components: {},
 };
 </script>
+
+<style>
+.home__heading {
+  margin-top: 4rem;
+  font-size: 3.2rem;
+  text-align: center;
+  color: var(--secondary-color);
+}
+</style>
