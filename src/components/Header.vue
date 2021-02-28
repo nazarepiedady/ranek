6b6f375aeb5c50ccf5ad95562116b5ab610e810a
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-28 14:26:38 +0100

    Add markup structure to Header
    Create a navigation with three links made using
    router-link component, and set the name and add
    computed properties to the Header component.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,29 @@
-<template></template>
+<template>
+  <header>
+    <nav class="nav">
+      <router-link to="/" class="logo">
+        <img src="@/assets/ranek.svg" alt="Ranek Logo" class="logo__img" />
+      </router-link>
+
+      <router-link to="/user" class="button">{{ name }}</router-link>
+
+      <router-link to="/login" class="button">Vender / Login</router-link>
+    </nav>
+  </header>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "Header",
+  computed: {
+    name() {
+      return this.$store.state.user.name.replace(/\s.*/, "");
+    },
+    login() {
+      return this.$store.state.login;
+    },
+  },
+};
 </script>
 
 <style>
