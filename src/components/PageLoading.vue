85256babcc464f7056e8bf6a2b4be66888fb73ac
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-02 16:30:21 +0100

    Write the loading markup structure
    It consist in a div with a class .loading
    which has four span element as child that
    will be animatelly translate to up and down
    with different delay each one.

--- src/components/PageLoading.vue
+++ src/components/PageLoading.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <div class="loading">
+    <span class="circle c1"></span>
+    <span class="circle c2"></span>
+    <span class="circle c3"></span>
+    <span class="circle c4"></span>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
