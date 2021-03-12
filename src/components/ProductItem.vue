7a81282f21269799f5f3192ad8172ba666fc14b6
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-12 22:14:45 +0100

    Populate product item component
    Show it if there's product data in the
    product props

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="product">
+  <div class="product" v-if="product">
     <router-link :to="{ name: 'Product', params: { id: product.id } }">
-      <img class="product__img" />
+      <img
+        v-if="product.photos"
+        :src="product.photos[0].src"
+        :alt="product.photos[0].name"
+        class="product__img"
+      />
       <p>Ver Produto</p>
     </router-link>
     <div class="product__info">
