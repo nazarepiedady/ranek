2c5950eea2215072cb7a7526860f440e0d949c65
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-12 19:49:11 +0100

    Create user form component
    To register, edit, login user

---
+++ src/components/UserForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="form">
+    <div class="user">
+      <label for="name">Nome</label>
+      <input type="text" id="name" name="name" />
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email" />
+      <label for="password">Senha</label>
+      <input type="password" id="password" name="password" />
+    </div>
+    <label for="country">País</label>
+    <input type="text" id="country" name="country" />
+    <div class="button-container">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
