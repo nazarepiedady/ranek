c3cee52e7e0ee5a776abde1e45a53c1858aee468
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-12 22:02:55 +0100

    Create product item component
    Add its markup and define some classes

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="product">
+    <router-link :to="{ name: 'Product', params: { id: product.id } }">
+      <img class="product__img" />
+      <p>Ver Produto</p>
+    </router-link>
+    <div class="product__info">
+      <p class="product__price"></p>
+      <h2 class="product__name"></h2>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
