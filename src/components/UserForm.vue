89b44501b3af3229d773b4b2464a68fe99d5952a
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-12 21:33:22 +0100

    Add style to user form component

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -2,14 +2,14 @@
   <form class="form" v-if="showLoginFields">
     <div class="user">
       <label for="name">Nome</label>
-      <input type="text" id="name" name="name" />
+      <input type="text" id="name" name="name" v-model="name" />
       <label for="email">Email</label>
-      <input type="email" id="email" name="email" />
+      <input type="email" id="email" name="email" v-model="email" />
       <label for="password">Senha</label>
-      <input type="password" id="password" name="password" />
+      <input type="password" id="password" name="password" v-model="password" />
     </div>
     <label for="country">País</label>
-    <input type="text" id="country" name="country" />
+    <input type="text" id="country" name="country" v-model="country" />
     <div class="button-container">
       <slot></slot>
     </div>
@@ -35,4 +35,19 @@ export default {
 </script>
 
 <style>
+.form,
+.user {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 8rem 1fr;
+}
+
+.user {
+  grid-column: 1 / 3;
+}
+
+.button-container {
+  grid-column: 2;
+  margin-top: 1rem;
+}
 </style>
