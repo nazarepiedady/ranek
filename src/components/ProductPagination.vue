948d228f488f88203a8dbf327aeb302b76f88731
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 12:27:38 +0100

    Create product pagination component
    Define name, props and its property types

---
+++ src/components/ProductPagination.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+export default {
+  name: "ProductPagination",
+  props: {
+    productPerPage: {
+      type: Number,
+      default: 1,
+    },
+    productTotal: {
+      type: Number,
+      default: 1,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
