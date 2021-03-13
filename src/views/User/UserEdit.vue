4bb0c827940748eb819734a841d6618c9896fb14
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 23:32:14 +0100

    Create update user method
    Add it to user edit view component

--- src/views/User/UserEdit.vue
+++ src/views/User/UserEdit.vue
@@ -9,7 +9,24 @@
 </template>
 
 <script>
-export default {};
+import { api } from "@/services.js";
+import UserForm from "@/components/UserForm.vue";
+
+export default {
+  name: "UserEdit",
+  components: { UserForm },
+  methods: {
+    updateUser() {
+      api
+        .put(`/user/${this.$store.state.user.id}`, this.$store.state.user)
+        .then(() => {
+          this.$store.dispatch("getUser", this.$store.state.user.id);
+          this.$router.push({ name: "User" });
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
 </script>
 
 <style>
