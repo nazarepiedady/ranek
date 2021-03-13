79ba0a18a8b8c947f4d84633244a76af72fc9ebf
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 23:11:35 +0100

    Add user component markup

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,4 +1,30 @@
-<template></template>
+<template>
+  <section class="user">
+    <nav class="sidebar">
+      <ul>
+        <li>
+          <router-link :to="{ name: 'User' }">Produtos</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'UserShop' }">Compras</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'UserSell' }">Vendas</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'UserEdit' }">Editar Usuario</router-link>
+        </li>
+        <li>
+          <button @click.prevent="logout">Deslogar</button>
+        </li>
+      </ul>
+    </nav>
+
+    <transition mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </section>
+</template>
 
 <script>
 export default {};
