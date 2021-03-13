a0cd84b3337dd08ed8be20f017ea3e3bcf84034b
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 23:46:07 +0100

    Add user sell view component markup

--- src/views/User/UserSell.vue
+++ src/views/User/UserSell.vue
@@ -1,4 +1,25 @@
-<template></template>
+<template>
+  <div v-if="sellList">
+    <h2>Vendas</h2>
+    <div class="product-wrapper" v-for="(sell, index) in sellList" :key="index">
+      <ProductItem v-if="sell.product" :product="sell.product">
+        <p class="seller">
+          <span>Comprador:</span>
+          {{ sell.customer_id }}
+        </p>
+      </ProductItem>
+
+      <div class="delivery">
+        <h3>Entrega:</h3>
+        <ul v-if="sell.address">
+          <li v-for="(value, key) in sell.address" :key="key">
+            {{ key }}: {{ value }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
