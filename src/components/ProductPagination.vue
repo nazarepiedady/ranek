805d237fdf217b24fe5ad5585fb0a70c6f555f34
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 12:35:23 +0100

    Create pages computed property
    to determine existed pages

--- src/components/ProductPagination.vue
+++ src/components/ProductPagination.vue
@@ -21,6 +21,24 @@ export default {
       };
     },
   },
+  computed: {
+    pages() {
+      const current = +this.$route.query.page;
+      const range = 9;
+      const offset = Math.ceil(range / 2);
+      const total = this.totalPage;
+      const pagesArray = [];
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i);
+      }
+
+      pagesArray.splice(0, current - offset);
+      pagesArray.splice(range, total);
+
+      return pagesArray;
+    },
+  },
 };
 </script>
 
