04df63a4ba2867521095ce3c6c28914156c53176
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-14 00:50:41 +0100

    Create user shop view markup

--- src/views/User/UserShop.vue
+++ src/views/User/UserShop.vue
@@ -1,4 +1,22 @@
-<template></template>
+<template>
+  <section>
+    <div v-if="shoppingList">
+      <h2>Compras</h2>
+      <div
+        class="product-wrapper"
+        v-for="(shopping, index) in shoppingList"
+        :key="index"
+      >
+        <ProductItem v-if="shopping.product" :product="shopping.product">
+          <p class="seller">
+            <span>Vendedor:</span>
+            {{ shopping.seller_id }}
+          </p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {};
