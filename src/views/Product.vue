e55e5b77ef71202785c71a312257c4aa09241076
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 17:25:21 +0100

    Create product view markup

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,4 +1,28 @@
-<template></template>
+<template>
+  <section>
+    <div v-if="product" class="product">
+      <ul class="gallery" v-if="product.photos > 0">
+        <li v-for="(photo, index) in product.photos" :key="index">
+          <img :src="photo.src" :alt="photo.name" />
+        </li>
+      </ul>
+
+      <div class="product__info">
+        <h1>{{ product.name }}</h1>
+        <p class="product__price">{{ product.price | toMoney }}</p>
+        <p class="product__description">{{ product.description }}</p>
+        <transition v-if="product.sold === 'false'" mode="out-in">
+          <button class="button" v-if="!finish" @click="finish = true">
+            Comprar
+          </button>
+          <CompletePurchase v-else :product="product" />
+        </transition>
+        <button class="button" disabled v-else>Produto vendido</button>
+      </div>
+    </div>
+    <PageLoading v-else />
+  </section>
+</template>
 
 <script>
 export default {};
