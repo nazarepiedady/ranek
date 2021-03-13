b4500958e576c417305d4be2e2c15a87f49c25ff
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-13 15:37:17 +0100

    Add markup to add product component

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,4 +1,22 @@
-<template></template>
+<template>
+  <form class="add-product-form">
+    <label for="name">Nome</label>
+    <input type="text" id="name" name="name" v-model="product.name" />
+    <label for="price">Preço (USD)</label>
+    <input type="number" name="price" id="price" v-model="product.price" />
+    <label for="photos">Fotos</label>
+    <input type="file" id="photos" name="photos" ref="photos" />
+    <label for="description">Descrição</label>
+    <textarea
+      name="description"
+      id="description"
+      v-model="product.description"
+    ></textarea>
+    <button class="button" @click.prevent="addProduct">
+      Adicionar Produto
+    </button>
+  </form>
+</template>
 
 <script>
 export default {};
