3f871a298dc73551b6da4d4556dffe15fac5f048
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-14 01:34:04 +0100

    Add login, user, user_product states to view

--- src/views/User/UserProduct.vue
+++ src/views/User/UserProduct.vue
@@ -3,8 +3,8 @@
     <h2>Adicionar Produtos</h2>
     <AddProduct />
     <h2>Seus Produtos</h2>
-    <transition-group v-if="userProducts" name="list" tag="ul">
-      <li v-for="(product, index) in userProducts" :key="index">
+    <transition-group v-if="user_products" name="list" tag="ul">
+      <li v-for="(product, index) in user_products" :key="index">
         <ProductItem :product="product">
           <p>{{ product.description }}</p>
           <button class="delete" @click.prevent="deleteProduct(product.id)">
@@ -17,7 +17,21 @@
 </template>
 
 <script>
-export default {};
+import { api } from "@/services.js";
+import { mapState, mapActions } from "vuex";
+import AddProduct from "@/components/AddProduct.vue";
+import ProductItem from "@/components/ProductItem.vue";
+
+export default {
+  name: "UserProduct",
+  components: {
+    AddProduct,
+    ProductItem,
+  },
+  computed: {
+    ...mapState(["login", "user", "user_products"]),
+  },
+};
 </script>
 
 <style>
