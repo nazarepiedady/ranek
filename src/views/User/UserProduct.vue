b11eb1a1fbe8fce5069c8a6aea479aaad20c94d4
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-14 01:28:01 +0100

    Create use product view markup

--- src/views/User/UserProduct.vue
+++ src/views/User/UserProduct.vue
@@ -1,4 +1,20 @@
-<template></template>
+<template>
+  <section>
+    <h2>Adicionar Produtos</h2>
+    <AddProduct />
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="userProducts" name="list" tag="ul">
+      <li v-for="(product, index) in userProducts" :key="index">
+        <ProductItem :product="product">
+          <p>{{ product.description }}</p>
+          <button class="delete" @click.prevent="deleteProduct(product.id)">
+            Deletar
+          </button>
+        </ProductItem>
+      </li>
+    </transition-group>
+  </section>
+</template>
 
 <script>
 export default {};
