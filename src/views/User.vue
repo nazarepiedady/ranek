33fc63840e010907bb3966e549d7f86c78747e91
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-13 21:16:10 +0100

    chore: translate text of `src/views/user.vue` to english

--- src/views/User.vue
+++ src/views/User.vue
@@ -3,19 +3,19 @@
     <nav class="sidebar">
       <ul>
         <li>
-          <router-link :to="{ name: 'User' }">Produtos</router-link>
+          <router-link :to="{ name: 'User' }">Products</router-link>
         </li>
         <li>
-          <router-link :to="{ name: 'UserShop' }">Compras</router-link>
+          <router-link :to="{ name: 'UserShop' }">Shopping</router-link>
         </li>
         <li>
-          <router-link :to="{ name: 'UserSell' }">Vendas</router-link>
+          <router-link :to="{ name: 'UserSell' }">Sales</router-link>
         </li>
         <li>
-          <router-link :to="{ name: 'UserEdit' }">Editar Usuario</router-link>
+          <router-link :to="{ name: 'UserEdit' }">Edit User</router-link>
         </li>
         <li>
-          <button @click.prevent="logout">Deslogar</button>
+          <button @click.prevent="logout">Logout</button>
         </li>
       </ul>
     </nav>
