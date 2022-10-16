69aa2a3ac3d6ae7d0dc1170449065cad20164ab1
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-10-16 21:57:13 +0100

    chore: translate `components/userform.vue` to english

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,14 +1,14 @@
 <template>
   <form class="form" v-if="showLoginFields">
     <div class="user">
-      <label for="name">Nome</label>
+      <label for="name">Name</label>
       <input type="text" id="name" name="name" v-model="name" />
       <label for="email">Email</label>
       <input type="email" id="email" name="email" v-model="email" />
-      <label for="password">Senha</label>
+      <label for="password">Password</label>
       <input type="password" id="password" name="password" v-model="password" />
     </div>
-    <label for="country">País</label>
+    <label for="country">Country</label>
     <input type="text" id="country" name="country" v-model="country" />
     <div class="button-container">
       <slot></slot>
