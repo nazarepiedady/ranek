e68477fd37d8eadbadd405f27ea9c319d0467492
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-13 21:19:48 +0100

    chore: move `map fields` code into its own file

---
+++ src/utils/map-fields.js
@@ -0,0 +1,18 @@
+export function mapFields(options) {
+  const object = {};
+  
+  for (let i = 0; i < options.fields.length; i++) {
+    const field = [options.fields[i]];
+
+    object[field] = {
+      get() {
+        return this.$store.state[options.base][field];
+      },
+      set(value) {
+        this.$store.commit(options.mutation, { [field]: value });
+      }
+    };
+  }
+
+  return object;
+}
